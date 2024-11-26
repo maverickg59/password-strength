2b0cb88c649f16bac8e73528ed9c6e907a496b81
Chris White <chris@chriswhite.rocks>
2024-11-26 08:06:08 -0800

    adds period missing in text

--- src/App.tsx
+++ src/App.tsx
@@ -15,14 +15,14 @@ function App() {
               <a href="https://github.com/dropbox/zxcvbn">
                 the password strength estimator, zxcvbn
               </a>
-              {""} The logic in zxcvbn uses common password patterns to estimate
-              the strength of a password. It's not a tool that's intended for
-              every day use. Instead, this tool provides a visual representation
-              of password strength to help users understand the difference
-              between weak and strong passwords and passphrases. Keep in mind
-              that no password is truly safe and all passwords can eventually be
-              cracked. Strong password hygiene is a matter of making that
-              exercise as difficult as possible for attackers.
+              {""}. The logic in zxcvbn uses common password patterns to
+              estimate the strength of a password. It's not a tool that's
+              intended for every day use. Instead, this tool provides a visual
+              representation of password strength to help users understand the
+              difference between weak and strong passwords and passphrases. Keep
+              in mind that no password is truly safe and all passwords can
+              eventually be cracked. Strong password hygiene is a matter of
+              making that exercise as difficult as possible for attackers.
             </p>
             <br />
             <p>
